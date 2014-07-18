5ccf7575fc5ee2cc9a927097c7349805d4fe847a
Chris Johnsen <cjohnsen@weareratio.com>
2014-07-18 16:57:08 -0700

    refactor: add constructors

--- js/main.js
+++ js/main.js
@@ -1,23 +1,47 @@
 var theremin = function() {
 	"use strict";
 
-	// params
-	var isFirefox = navigator.userAgent.toLowerCase().indexOf('firefox') > -1,
-		isTouch = 'ontouchstart' in window,
-		x = 0,
-		y = 0;
+	/** config params */
+	var isTouch = 'ontouchstart' in window,
+		isFirefox = navigator.userAgent.toLowerCase().indexOf('firefox') > -1;
 
-	// audio params
+	/** audio params */
 	var context,
 		oscillator,
 		type = 'SINE',
-		gainNode,
-		gain,
-		frequency,
 		convolver,
 		duration = 0,
 		decay = 0;
 
+	/** constructors */
+	var Oscillator = function() {
+		this.osc = context.createOscillator();  // create sound source
+		this.amp = context.createGain();
+		this.osc.connect(this.amp);
+		this.amp.connect(context.destination);  // connect sound to speakers
+		this.osc.type = isFirefox ? type.toLowerCase() : this.osc[type.toUpperCase()];
+	}
+
+	var Convolver = function() {
+		this.convolver = context.createConvolver();
+		this.convolver.buffer = new ImpulseResponse();
+		oscillator.osc.connect(this.convolver);
+		this.convolver.connect(context.destination);
+	}
+
+	var ImpulseResponse = function() {
+		var sampleRate = context.sampleRate,    // audio data in sample-frames per second
+			length = sampleRate * duration,     // buffer size in sample-frames
+			impulse = context.createBuffer(1, length, sampleRate),   // 1 channel = mono
+			channel = impulse.getChannelData(0);
+
+		for (var i = 0; i < length; i++) {
+			channel[i] = (Math.random() * 2 - 1) * Math.pow(1 - i / length, decay);
+		}
+		return impulse;
+	}
+
+	/** methods */
 	var init = function() {
 		context = new (window.AudioContext || window.webkitAudioContext)();
 		bindEvents();
@@ -31,9 +55,9 @@ var theremin = function() {
 			stage.addEventListener("touchend", off);
 			stage.addEventListener("touchmove", modulate);
 			/*stage.addEventListener("touchmove", function(e) {
-				e.preventDefault();
-				modulate();
-			});*/
+			 e.preventDefault();
+			 modulate();
+			 });*/
 		} else {
 			stage.addEventListener("mouseover", on);
 			stage.addEventListener("mouseout", off);
@@ -50,28 +74,16 @@ var theremin = function() {
 	}
 
 	var on = function(e) {
-		console.log('on');
-		oscillator = context.createOscillator();    // create sound source (single-use entities)
-		oscillator.type = isFirefox ? type.toLowerCase() : oscillator[type.toUpperCase()];
-
-		gainNode = context.createGain();
-		oscillator.connect(gainNode);
-		gainNode.connect(context.destination);  // connect sound to speakers
-
-		if (duration) {
-			convolver = context.createConvolver();
-			convolver.buffer = impulseResponse(duration, decay);
-			oscillator.connect(convolver);
-			convolver.connect(context.destination);
-		}
-
-		oscillator.start(context.currentTime);
+		oscillator = new Oscillator();
+		oscillator.osc.start(context.currentTime);
 		modulate(e);
+
+		if (duration)
+			convolver = new Convolver();
 	}
 
 	var off = function() {
-		console.log('off');
-		oscillator.stop(context.currentTime);
+		oscillator.osc.stop(context.currentTime);
 	}
 
 	var modulate = function(e) {
@@ -81,15 +93,17 @@ var theremin = function() {
 	}
 
 	var setGain = function(e) {
-		y = isTouch ? e.touches[0].pageY : e.clientY;
-		gain = 1 - y/window.innerHeight;
-		gainNode.gain.value = gain;
+		var y = isTouch ? e.touches[0].pageY : e.clientY,
+			gain = 1 - y/window.innerHeight;
+
+		oscillator.amp.gain.value = gain;
 	}
 
 	var setFrequency = function(e) {
-		x = isTouch ? e.touches[0].pageX : e.clientX;
-		frequency = (1705 * x/window.innerWidth) + 55;  // 5 octaves between A1 (55Hz) and A6 (1760Hz)  // 27.5 --> 4186
-		oscillator.frequency.value = Math.round(frequency);
+		var x = isTouch ? e.touches[0].pageX : e.clientX,
+			frequency = (1705 * x/window.innerWidth) + 55;  // 5 octaves between A1 (55Hz) and A6 (1760Hz)  // 27.5 --> 4186
+
+		oscillator.osc.frequency.value = Math.round(frequency);
 	}
 
 	var setWaveform = function(e) {
@@ -100,19 +114,7 @@ var theremin = function() {
 	var setReverb = function(e) {
 		duration = parseFloat(e.target.dataset.duration);
 		decay = parseFloat(e.target.dataset.decay);
-		console.log(e.target.textContent, duration, decay);
-	}
-
-	var impulseResponse = function(duration, decay) {
-		var sampleRate = context.sampleRate,    // audio data in sample-frames per second
-			length = sampleRate * duration,     // buffer size in sample-frames
-			impulse = context.createBuffer(1, length, sampleRate),   // 1 channel = mono
-			channel = impulse.getChannelData(0);
-
-		for (var i = 0; i < length; i++) {
-			channel[i] = (Math.random() * 2 - 1) * Math.pow(1 - i / length, decay);
-		}
-		return impulse;
+		console.log(e.target.textContent);
 	}
 
 	return {
